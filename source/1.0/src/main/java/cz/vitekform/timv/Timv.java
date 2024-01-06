aee5a8468c30a7151150f285d1f7c72f0363c15c
Vít Formánek <vit.formanek.2022b@gypce.cz>
2024-01-06 13:23:20 +0000

    Update

--- source/1.0/src/main/java/cz/vitekform/timv/Timv.java
+++ source/1.0/src/main/java/cz/vitekform/timv/Timv.java
@@ -1,17 +1,28 @@
 package cz.vitekform.timv;
 
+import cz.vitekform.timv.functions.datafunctions;
+import cz.vitekform.timv.functions.configfunctions;
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Timv extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        // Plugin startup logic
-
+        configfunctions ConfigFunctions = new configfunctions();
+        ConfigFunctions.setup();
+        FileConfiguration config = ConfigFunctions.get();
+        config.getDefaults().addDefault("minPlayersPerInstance", 10);
+        config.getDefaults().addDefault("maxPlayersPerInstance", 20);
+        ConfigFunctions.reload(config);
+        datafunctions DataFunctions = new datafunctions();
+        FileConfiguration data = DataFunctions.get();
+        DataFunctions.reload(data);
+        super.onEnable();
     }
 
     @Override
     public void onDisable() {
-        // Plugin shutdown logic
+        super.onDisable();
     }
 }
---
+++ source/1.0/src/main/java/cz/vitekform/timv/functions/configfunctions.java
@@ -0,0 +1,44 @@
+package cz.vitekform.timv.functions;
+
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
+
+import java.io.File;
+import java.io.IOException;
+
+public class configfunctions {
+
+    private static String dir;
+    private static File f;
+    private static FileConfiguration config;
+
+    public void setup(){
+        dir = System.getProperty("user.dir");
+        if (!new File(dir + "/plugins/BCMD").isDirectory()){
+            new File(dir + "/plugins/BCMD").mkdirs();
+        }
+        f = new File(dir + "/plugins/BCMD/config.yml");
+        config = YamlConfiguration.loadConfiguration(f);
+    }
+
+    public void reload(FileConfiguration config){
+        try {
+            config.save(f);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        setup();
+    }
+
+    public FileConfiguration get(){
+        return config;
+    }
+
+    public void save(FileConfiguration configuration){
+        try {
+            configuration.save(f);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
